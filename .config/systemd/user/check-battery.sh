148cff8a6ee0dbad5f6e662cfc19480f14e5d695
0ldude <geniodelletartarughe@protonmail.com>
2023-02-01 11:21:55 +0100

    check-battery script fix

--- .config/systemd/user/check-battery.sh
+++ .config/systemd/user/check-battery.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 printf 'script started' | systemd-cat -t check-battery #write to log when script is called
 BATTINFO=`acpi -b`
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:15:00 ]] ; then
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 4 -d " " | cut -f 1 -d "%"` < 15 ]] ; then
         printf 'condition is true' | systemd-cat -t check-battery #write to log if condition is true
         /usr/bin/dunstify -u critical "battery" "$BATTINFO"
 fi
