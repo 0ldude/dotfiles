aadea0abe16c8c96edcb3ccda5fde1b922ab31c4
andrecoron <a.corona30@studenti.unica.it>
2023-06-20 20:21:46 +0200

    forgot init.lua for light-dark theme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,7 +13,13 @@ require("variables")
 -- Plugins
 require("plugins")
 
-vim.cmd([[colorscheme gruvbox]])
+-- Set THEME globally
+local hr = tonumber(os.date('%H', os.time()))
+if hr > 6 and hr < 21 then THEME = "light"
+else THEME = "dark"
+end
+
+vim.cmd('colorscheme gruvbox')
 
 -- General and plugin-specific highlight-groups
 require("highlight-groups/nvim")
@@ -22,9 +28,12 @@ require("highlight-groups/nvim-notify")
 
 -- General and plugin-specific keymaps
 require("keymaps")
+require("keymaps/barbar")
 require("keymaps/icon-picker")
+--require("keymaps/iron")
 require("keymaps/nvim-tree")
 require("keymaps/telescope")
+require("keymaps/vim-textobj-hydrogen")
 
 -- General and plugin-specific options
 require("options")
@@ -35,22 +44,30 @@ require("options/cinnamon")
 require("options/Comment")
 require("options/fold-preview")
 require("options/gitsigns")
+require("options/glow")
 require("options/gruvbox")
 require("options/icon-picker")
 require("options/indent-blankline")
+--require("options/iron")
+require("options/jupyter")
+require("options/jupytext")
 require("options/lualine")
 require("options/mason")
+require("options/magma-nvim")
 require("options/neovim-session-manager")
 require("options/neorg")
 require("options/nvim-autopairs")
 require("options/nvim-cmp")
 require("options/nvim-colorizer")
+require("options/nvim-ipy")
 require("options/nvim-notify")
 require("options/nvim-tree")
+require("options/nvim-treesitter")
 require("options/nvim-web-devicons")
 require("options/packer")
 require("options/scrollbar")
 require("options/telescope")
 require("options/tidy")
+require("options/FTerm")
 require("options/trouble")
 require("options/true-zen")
